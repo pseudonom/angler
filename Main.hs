de906c0e2c8ead4503b609995a599ffe51790583
Eric Easley <escalier@riseup.net>
2014-02-24 05:03:58 -0500

    Pagespeed optimizations

--- Main.hs
+++ Main.hs
@@ -4,41 +4,42 @@
 module Main where
 
 
-import           Control.Applicative         ((<$>))
-import           Data.Foldable               (foldMap)
-import qualified Data.List                   as L (head)
-import           Data.Monoid                 ((<>))
-import           Prelude                     hiding (head, init, last, length,
-                                              tail, zipWith)
-import           System.Console.GetOpt       (ArgDescr (..), ArgOrder (..),
-                                              OptDescr (..), getOpt)
-import           System.Environment          (getArgs)
+import           Control.Applicative          ((<$>))
+import           Data.Foldable                (foldMap)
+import qualified Data.List                    as L (head)
+import           Data.Monoid                  ((<>))
+import           Prelude                      hiding (head, init, last, length,
+                                               tail, zipWith)
+import           System.Console.GetOpt        (ArgDescr (..), ArgOrder (..),
+                                               OptDescr (..), getOpt)
+import           System.Environment           (getArgs)
 
-import           Data.HashMap.Strict         (HashMap)
-import           Data.Text                   (Text, isInfixOf)
-import qualified Data.Text                   as T (head, tail)
-import           Data.Text.Encoding          (decodeUtf8)
-import           Data.Text.Lazy              (toStrict)
-import           Data.Vector                 (Vector, head, length, tail)
-import           Happstack.Lite              (Browsing (..), Method (..),
-                                              Response, ServerPart,
-                                              asContentType,
-                                              defaultServerConfig, dir,
-                                              getHeaderM, lookText, method,
-                                              msum, ok, port, serve,
-                                              serveDirectory, serveFile,
-                                              toResponse)
-import           Text.Blaze.Html5            (Html, toHtml, (!))
-import qualified Text.Blaze.Html5            as H
-import qualified Text.Blaze.Html5.Attributes as A
+import           Data.HashMap.Strict          (HashMap)
+import           Data.Text                    (Text, isInfixOf)
+import qualified Data.Text                    as T (head, tail)
+import           Data.Text.Encoding           (decodeUtf8)
+import           Data.Text.Lazy               (toStrict)
+import           Data.Vector                  (Vector, head, length, tail)
+import           Happstack.Lite               (Browsing (..), Method (..),
+                                               Response, ServerPart,
+                                               asContentType,
+                                               defaultServerConfig, dir,
+                                               getHeaderM, lookText, method,
+                                               msum, ok, port, serve,
+                                               serveDirectory, serveFile,
+                                               setHeaderM, toResponse)
+import           Happstack.Server.Compression (compressedResponseFilter)
+import           Text.Blaze.Html5             (Html, toHtml, (!))
+import qualified Text.Blaze.Html5             as H
+import qualified Text.Blaze.Html5.Attributes  as A
 
-import           Analyze                     (BadLang (..), Fail (..),
-                                              GoodLang (..), InLang (..),
-                                              TextInfo (..), WordInfo (..),
-                                              analyze)
-import           Etym                        (Lang (..), Origin, Target,
-                                              loadEtyms)
-import           Thesaurus                   (Thes, loadThes)
+import           Analyze                      (BadLang (..), Fail (..),
+                                               GoodLang (..), InLang (..),
+                                               TextInfo (..), WordInfo (..),
+                                               analyze)
+import           Etym                         (Lang (..), Origin, Target,
+                                               loadEtyms)
+import           Thesaurus                    (Thes, loadThes)
 
 main :: IO ()
 main = do
@@ -52,18 +53,29 @@ heroku = Option ['p'] [] (ReqArg read "PORT") "Port to serve"
 
 app :: HashMap Target Origin -> Thes -> ServerPart Response
 app e t = msum
-  [ dir "static" $ serveDirectory DisableBrowsing [] "static"
-  , dir "about" $ serveFile (asContentType "text/html") "static/about.html"
+  [ dir "static" $ do
+       expire
+       serveDirectory DisableBrowsing [] "static"
+  , dir "about" $ do
+       expire
+       serveFile (asContentType "text/html") "static/about.html"
   , angler e t
   ]
+
+expire :: ServerPart ()
+expire = setHeaderM "Cache-Control" "max-age=86400"
+-- compress :: ServerPart ()
+-- compress = setHeaderM "Content-Encoding" "gzip"
 
 data IsWebKit = WebKit | NotWebKit
 
 angler :: HashMap Target Origin -> Thes -> ServerPart Response
 angler e t = msum [input, output] where
   input = do method GET
+             expire
              serveFile (asContentType "text/html") "static/input.html"
   output = do method POST
+              _ <- compressedResponseFilter
               text <- toStrict <$> lookText "text"
               wk <- renderer <$> getHeaderM "User-Agent"
               ok . template . htmlify wk $ angle e t text where
@@ -72,14 +84,14 @@ angler e t = msum [input, output] where
                                    | otherwise = NotWebKit
 
 template :: Html -> Response
-template body = toResponse $
+template body = toResponse $ do
   H.docTypeHtml $ do
     H.head $ do
       H.title "Angler"
-      H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "static/angler.css"
-      H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "http://fonts.googleapis.com/css?family=Uncial+Antiqua"
       H.script ! A.type_ "text/javascript" $ js
     H.body ! A.class_ "marginalia" $ body
+  H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "static/angler.min.css"
+  H.link ! A.rel "stylesheet" ! A.type_ "text/css" ! A.href "http://fonts.googleapis.com/css?family=Uncial+Antiqua"
 
 htmlify :: IsWebKit -> Vector TextInfo -> Html
 htmlify wk ts = H.article ! A.class_ (cc wk) $ spanify first (head ts) <> foldMap (spanify toHtml) (tail ts) where
